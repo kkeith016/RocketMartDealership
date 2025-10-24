db86e874abbfd022c3fa9e4d6c8afad0a081fc8c
Kylee <kkeith.biz@gmail.com>
2025-10-24 13:51:27 -0400

    Dealership ArrayList

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,28 +1,57 @@
 package com.pluralsight;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
-    public class Dealership {
-        private String name;
-        private String address;
-        private String phone;
-        private List<Vehicle> inventory;
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
 
-        public Dealership(String name, String address, String phone) {
-            this.name = name;
-            this.address = address;
-            this.phone = phone;
-            this.inventory = new ArrayList<>();
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        inventory = new ArrayList<Vehicle>();
+    }
+
+    public void addVehicle(Vehicle v) {
+        inventory.add(v);
+    }
+
+    public boolean removeVehicleById(String id) {
+        for (int i = 0; i < inventory.size(); i++) {
+            if (inventory.get(i).getId().equals(id)) {
+                inventory.remove(i);
+                return true;
+            }
         }
-        public String getName() { return name; }
-        public String getAddress() { return address; }
-        public String getPhone() { return phone; }
-        public List<Vehicle> getInventory() { return inventory; }
+        return false;
+    }
 
-        public void addVehicle(Vehicle v) {
-            inventory.add(v);
+    public ArrayList<Vehicle> getAllVehicles() {
+        return inventory;
+    }
+
+    public ArrayList<Vehicle> findByMake(String make) {
+        ArrayList<Vehicle> results = new ArrayList<Vehicle>();
+        for (Vehicle v : inventory) {
+            if (v.getMake().equalsIgnoreCase(make)) {
+                results.add(v);
+            }
         }
+        return results;
+    }
 
+    public ArrayList<Vehicle> findByPriceRange(double min, double max) {
+        ArrayList<Vehicle> results = new ArrayList<Vehicle>();
+        for (Vehicle v : inventory) {
+            if (v.getPrice() >= min && v.getPrice() <= max) {
+                results.add(v);
+            }
+        }
+        return results;
+    }
+
+    // You can add more search methods similarly (year, type, color, mileage)
 }
